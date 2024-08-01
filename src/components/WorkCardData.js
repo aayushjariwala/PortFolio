64fdd617224235d6d52d8f9c45e87319ccfafaa5
Aayush Jariwala <aayushjariwala@Aayushs-MacBook-Pro.local>
2024-08-01 09:59:49 -0400

    1august

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,19 +1,22 @@
 import image from '../assets/Premium Vector _ Young programmer code for client projects developer writing program code flat vector.jpeg';
+import celebrations from '../assets/celebrations.png'
+import candle from '../assets/Screenshot 2024-07-30 at 1.30.35 PM.png'
+
 
 
 const WorkCardData = [
     {
-        imgsrc: image,
-        title: "E-Commerce website design",
-        text: "Thriving faacebook community and top ranked podcast to the Thriving faacebook community and top ranked podcast to theThriving faacebook community and top ranked podcast to the",
+        imgsrc: candle,
+        title: "Candle Store",
+        text: "MERN STACK website for selling candles with admin pannel to add and update products",
         view: "https://www.youtube.com"
     },
 
     {
-        imgsrc: image,
-        title: "E-Commerce website design",
-        text: "Thriving faacebook community and top ranked podcast to the Thriving faacebook community and top ranked podcast to theThriving faacebook community and top ranked podcast to the",
-        view: "https://www.youtube.com"
+        imgsrc: celebrations,
+        title: "Cake Shop",
+        text: "Fully working cake selling website developed using Wix studio",
+        view: "https://adoniscreed2412.wixsite.com/celebrations"
     },
 
     {
