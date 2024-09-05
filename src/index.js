ca2573f270f063d44d85239630b5e5e07a85159f
Aayush jariwala <aayushjariwala88@gmail.com>
2024-09-05 11:53:00 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/portFolio">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
